a69ed8a922c452fb0260dad01773d1cf91f6b108
davidli0025 <biaobiao0025@gmail.com>
2024-05-06 00:46:42 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    shift_amount %= 26
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
+    print(f"The {cipher_direction}d text is {end_text}")
+
+
+print(art.logo)
+
+is_game_continue = True
+while is_game_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    if input("Type 'yes' if you want to go again, Otherwise type 'no'.") == "no":
+        is_game_continue = False
+        print("Goodbye")
